0cbe4625406d62f992c510ec173a08cd1f4dfc77
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-11-12 17:46:46 +0700

    finish file writer

--- l03files/main.go
+++ l03files/main.go
@@ -10,17 +10,21 @@ import (
 func main() {
 
 	rootPath, err := os.Getwd()
+	filepath := rootPath + "/data/text.txt"
 
 	if err != nil {
 		fmt.Printf("ERROR: %v\n", err)
 		return
 	}
 
-	c, err := fileutils.ReadTextFile(rootPath + "/data/text.txt")
+	c, err := fileutils.ReadTextFile(filepath)
 
 	if err != nil {
 		fmt.Printf("ERROR PANIC!!: %v\n", err)
 	} else {
 		fmt.Println(c)
+		newContent := fmt.Sprintf("Original: %v\nDouble Original:\n%v\n%v\n", c, c, c)
+
+		fileutils.WriteToFile(filepath+".output.txt", newContent)
 	}
 }
